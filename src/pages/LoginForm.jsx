f0f9f488c6b7a6599d3ff85b91b285e81fdd3284
lepidodendronnnn <alexandrevnas@gmail.com>
2025-01-18 14:55:22 +0300

    Initial commit

---
+++ src/pages/LoginForm.jsx
@@ -0,0 +1,23 @@
+import {useContext, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
+export default function LoginForm() {
+    /*
+    const {login} = useContext(UserContext)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate();*/
+
+    return (<div> <h2>HELP</h2></div>)
+}
+
+/*
+function sendLogin(email, password){
+    return fetch('/api/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email, password})
+    })
+        .then()
+}
+*/
